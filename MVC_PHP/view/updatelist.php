aac70c6c434161671583c40118a56563e80aeb3b
Kelsey Cossifos <kcossifos@gmail.com>
2017-03-05 19:18:04 -0500

    updated files

--- MVC_PHP/view/updatelist.php
+++ MVC_PHP/view/updatelist.php
@@ -2,9 +2,9 @@
 $todo = $_GET['id'];
 echo "<a id='back' href='?action=home'>Go Back</a>" .
 "<form action='?action=updateItem&id={$todo}' method='post'>
-	Title: <input type='text' name='item' value='{$results[0]['Todo']}'>
-	Description: <input type='text' name='desc' value='{$results[0]['descrip']}'>
-	Date: <input type='text' name='dates' value='{$results[0]['date']}'>
+	Title: <input type='text' name='item' value='{$results[0]['Todo']}' required>
+	Description: <input type='text' name='desc' value='{$results[0]['descrip']}' required>
+	Date: <input type='text' name='dates' value='{$results[0]['date']}' required>
 	       <input id='button' type='submit' name='submit' value='Update Now'/>
 </form> "
 ?>
