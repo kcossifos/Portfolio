257556229eb1bd0204ad2c5f23fc0bbe9f68649c
Kelsey Cossifos <kcossifos@gmail.com>
2017-03-10 21:25:35 -0500

    Revert "small edits"
    This reverts commit 3388e9c2019dd1413317070b00f15ed2cbdba6ec.

--- ToDoList/models/getList.php
+++ ToDoList/models/getList.php
@@ -1,9 +1,7 @@
 <?php
 include('includes/db.php');
-//selects only one of the rows
 $stmt = $dbh->prepare("select Todo, descrip, date from PhpMVC order by rand() limit 1");
 $stmt->execute();
 $result = $stmt->fetchAll();
-//returns a string that contains the json value
 $result = json_encode($result);
 ?>
